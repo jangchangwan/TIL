9f8ae4fcc54badacb342482edbe8141f978be6b8
jangchangwan <tktkd6755@gmail.com>
2022-02-25 02:11:57 +0900

    1 day 1 beakjoon

---
+++ docs/03_baekjoon/2477_참외밭/s1.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+N = int(input())
+
+
+data_lst = list()
+
+max_row = max_col = 0
+for i in range(6):
+    # 데이터 받아오기
+    new_dir, new_dis = map(int, input().split())
+    data_lst.append((new_dir, new_dis))
+    # 최대 가로 세로 길이 구하기
+    if data_lst[i][0] == 1 or data_lst[i][0] == 2:
+        if data_lst[i][1] > max_col:
+            max_col = data_lst[i][1]
+            max_col_idx = i
+    elif data_lst[i][0] == 3 or data_lst[i][0] == 4:
+        if data_lst[i][1] > max_row:
+            max_row = data_lst[i][1]
+            max_row_idx = i
+
+if max_row_idx > max_col_idx:
+    min_row = data_lst[(max_row_idx+2) % 6][1]
+    min_col = data_lst[(max_col_idx+4) % 6][1]
+else :
+    min_row = data_lst[(max_row_idx + 4) % 6][1]
+    min_col = data_lst[(max_col_idx + 2) % 6][1]
+
+ans = ((max_row * max_col) - (min_row * min_col)) * N
+print(ans)
