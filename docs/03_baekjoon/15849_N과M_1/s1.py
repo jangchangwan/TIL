41b51a82986da8e6a4a0fea5656fa53e2528e744
jangchangwan <tktkd6755@gmail.com>
2022-02-28 20:43:13 +0900

    1day 1baekjoon

---
+++ docs/03_baekjoon/15849_N과M_1/s1.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+
+def back_T():
+    global N, M
+    if len(stack) == M: #
+        print(*stack)
+    else:
+        for i in range(1, N + 1):
+            if i in stack:
+                continue
+            stack.append(i)
+            back_T()
+            stack.pop()
+
+
+N, M = map(int, input().split())
+stack = list()
+back_T()
