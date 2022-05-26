264f0982587613ee7a5a86ad1a7fca310e1cf5c7
jangchangwan <tktkd6755@gmail.com>
2022-05-26 21:56:45 +0900

    push algo problem

--- docs/03_baekjoon/1000/1107_리모콘/s1.py
+++ docs/03_baekjoon/1000/1107_리모콘/s1.py
@@ -1,29 +1,33 @@
 # 1107_리모콘_문제풀이
-# 2022-03-30
-# EOFError
+# 2022-05-26
+
 import sys
 sys.stdin = open('input.txt', 'r')
 
-
-start_num = 100
-
-end_num = int(input())
+N = int(input())
 M = int(input())
-button = list(map(int, input().split()))
 
-cnt = abs(start_num - end_num)
-for i in range(1000001):
-    temp = str(i)
-    state = False
+if M:
+    M_lst = list(map(int, input().split()))
+else:
+    M_lst = []
 
-    for j in temp:
-        if int(j) in button:
-            state = True
+# 채널 100에서 시작하므로 최대값
+min_ans = abs(N - 100)
+
+# 범위를 100만으로 한이유
+# 40만에서 시작해서 50만까지갔을경우보다
+# 53만에서 50만 가는 경우가 최적해에 가깝기때문에
+# 최대 100만까지는 돌려야할것이다.
+
+for num in range(0, 1000001):
+    num = str(num)
+
+    for j in range(len(num)):
+        # check
+        if int(num[j]) in M_lst:
             break
-        elif state:
-            continue
+        elif j == len(num) - 1:
+            min_ans = min(min_ans, abs(int(num) - N) + len(num))
 
-    else:
-        cnt = min(cnt, abs(end_num-i) + len(str(i)))
-
-print(cnt)
+print(min_ans)
