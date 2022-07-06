9456510a4dec5c8bb4a1b728e6d5b69b59bb9a00
jangchangwan <tktkd6755@gmail.com>
2022-07-06 22:32:39 +0900

    push algo problem

---
+++ docs/03_baekjoon/math/19698_헛간 청약/s1.py
@@ -0,0 +1,17 @@
+# 11967_불켜기
+# 2022-07-06
+
+
+def BFS(sr, sc):
+    pass
+
+
+# 입력
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+# 탐색색
+answer = BFS(0, 0)
+
+# 출력
+print(answer)
