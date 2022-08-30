f808ef0abc9fe64fe862034da9be19fe25766b68
jangchangwan <tktkd6755@gmail.com>
2022-08-30 23:02:06 +0900

    push algo problem

---
+++ docs/03_baekjoon/implementation/4714_Lunacy/s1.py
@@ -0,0 +1,9 @@
+# 4714_Lunacy
+# 2022-08-23
+
+
+while True:
+    N = float(input())
+    if N < 0:
+        break
+    print(f'Objects weighing {N:.2f} on Earth will weigh {N*0.167:.2f} on the moon.')
