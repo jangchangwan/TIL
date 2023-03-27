4d5fe5c5e6471e3ccb789de1a327550c9e915f53
jangchangwan <tktkd6755@gmail.com>
2023-03-27 09:06:46 +0900

    push algo

---
+++ docs/03_baekjoon/implementation/27889_특별한 학교 이름.py
@@ -0,0 +1,12 @@
+
+
+chang_dict = {
+    'NLCS': 'North London Collegiate School',
+    'BHA': 'Branksome Hall Asia',
+    'KIS': 'Korea International School',
+    'SJA': 'St. Johnsbury Academy',
+}
+
+name = input()
+
+print(chang_dict[name])
