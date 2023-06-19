233a286ce2c072caa51c2649ae00230520f07d0c
jangchangwan <tktkd6755@gmail.com>
2023-06-19 09:16:12 +0900

    push algo

---
+++ docs/03_baekjoon/Dp/11052_카드 구매하기.py
@@ -0,0 +1,10 @@
+N = int(input())
+price_list = [0] + list(map(int, input().split()))
+dp = [0 for _ in range(N+1)]
+
+
+for i in range(1,N+1):
+    for k in range(1,i+1):
+        dp[i] = max(dp[i], dp[i-k] + price_list[k])
+
+print(dp[i])
