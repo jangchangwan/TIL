a26e759f7e6987ce3f1ce386766de62bb4a6e0c0
jangchangwan <tktkd6755@gmail.com>
2023-06-28 10:26:35 +0900

    push algo

---
+++ docs/03_baekjoon/prefix_sum/14944_굿점원.py
@@ -0,0 +1,26 @@
+N, M = map(int, input().split())
+    
+distance = list(map(int, input().split()))
+prefix_sum_distance = [0] * (N + 1)
+prefix_sum_distance[1] = distance[0]
+answer = 2000000000
+for i in range(1, N+1):
+    prefix_sum_distance[i] = prefix_sum_distance[i-1] + distance[i-1]
+
+
+print(prefix_sum_distance)
+for _ in range(M):
+    start, end, dis = map(int, input().split())
+    if start > end:
+        start, end = end, start
+        
+    if (end - start == 1):
+        answer = min(answer, prefix_sum_distance[start] + dis + (prefix_sum_distance[N] - prefix_sum_distance[end]))
+    
+    answer = min(answer, prefix_sum_distance[start] + dis + (prefix_sum_distance[N] - prefix_sum_distance[end]) + (prefix_sum_distance[N] - prefix_sum_distance[start + 1]))
+    answer = min(answer, dis * 2 + (prefix_sum_distance[N] - prefix_sum_distance[end]) * 2 + prefix_sum_distance[end - 1])
+    answer = min(answer, prefix_sum_distance[end] + dis + prefix_sum_distance[N] - prefix_sum_distance[start])
+    answer = min(answer, dis * 2 + prefix_sum_distance[N])
+    print(start, end)
+    
+print(answer)
